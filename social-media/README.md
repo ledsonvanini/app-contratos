6f67d77b7ee292c3c8e15c2e402456abc41e83a6
Ledson Vanini <ledsonvanini@gmail.com>
2025-03-06 17:19:49 +0000

    feat: social media start

---
+++ social-media/README.md
@@ -0,0 +1,9 @@
+### Social Media
+
+## Commands
+- npm create vite@latest social-media
+- npm i @supabase/supabase-js @tanstack/react-query
+- npm i tailwindcss @tailwindcss/vite
+- npm i react-router
+
+
---
+++ social-media/src/main.tsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+import { BrowserRouter as Router } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const client = new QueryClient(); // Query cache, 
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={client}>
+
+      <Router>
+        <App />
+      </Router>
+
+    </QueryClientProvider>
+  </StrictMode>,
+)
